eb1bb85c4a339427388d2b929fe64cf206333a69
subhash kumar mandal <pm817405@gmail.com>
2025-09-26 21:22:32 +0530

    check months

---
+++ control-flow/basis-2/index.js
@@ -0,0 +1,57 @@
+// --------------------------
+// 🔹 Else-if Practice
+// --------------------------
+
+// 1. check number positive ,negative , zero . 
+let num = 1000;
+if (num>0){
+    console.log("its positive")
+}else if(num===0){
+    console.log("its zero")
+}else{
+    console.log("its negative")
+}
+
+// 2.check grade system . 
+let marks = 100 ;
+
+if(marks>=90 && marks<=100){
+    console.log("A")
+}else if(marks>=80 && marks<=89){
+    console.log("B")
+}else if(marks>=70 && marks<=79){
+    console.log("C")
+}else if(marks>=60 && marks<=69){
+    console.log("D")
+}else{
+    console.log("Fail")
+}
+
+// 3. check the month 
+let monthnum = 12;
+
+if(monthnum ===1){
+    console.log("January")
+} else if(monthnum===2){
+    console.log("February")
+}else if(monthnum ===3){
+    console.log("March")
+}else if(monthnum ===4){
+    console.log("April")
+}else if(monthnum ===5){
+    console.log("May")
+}else if(monthnum ===6){
+    console.log("June")
+}else if(monthnum ===7){
+    console.log("July")
+}else if(monthnum ===8){
+    console.log("August")
+}else if(monthnum ===9){
+    console.log("August")
+}else if(monthnum ===10){
+    console.log("October")
+}else if(monthnum ===11){
+    console.log("November")
+}else if(monthnum ===12){
+    console.log("December")
+}
