0ae39d1703392964fd8b8326fbd9ab7b1dd86317
subhash kumar mandal <pm817405@gmail.com>
2025-09-26 20:54:03 +0530

    if else

---
+++ control-flow/basics-1/index.js
@@ -0,0 +1,36 @@
+// --------------------
+// basics control flow
+// --------------------
+
+// 1. check number positive ya negative . 
+let num = 10 ;
+if(num>0){
+    console.log("its positive number")
+}else{
+    console.log("its negative number")
+}
+
+
+// 2. check number even ya odd . 
+ let num1 = 13 ;
+if(num1%2===0){
+    console.log("even number")
+}else{
+    console.log("odd number")
+}
+
+// 3. check voting eligibleti
+let num2 = 17 
+if(num2>=18){
+    console.log("voting eligible")
+}else{
+    console.log("not eligible")
+}
+
+// 4. check fill ya not 
+let str = "hello";
+if(str ===""){
+    console.log("string is emty")
+}else{
+    console.log("sting is fill")
+}
