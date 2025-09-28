e413ec1055efb8f973b7878b4b1a85195478d4ee
subhash kumar mandal <pm817405@gmail.com>
2025-09-28 12:35:30 +0530

    question 4

--- For-loop/level-1/index.js
+++ For-loop/level-1/index.js
@@ -14,3 +14,16 @@ for (let i =1 ;i<=100;i++){
         console.log(i)
     }
 }
+
+// 3. 1 to 50 print all odd number . 
+for(let i=1;i<=50;i++){
+    if(i%2!==0){
+        console.log(i)
+    }
+}
+
+// 4. 10 to  1  reverse  numbera print . 
+for(let i= 10;i>=1;i--){
+    console.log(i)
+}
+
