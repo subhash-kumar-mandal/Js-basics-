c6f3f87babb962fc1c0427da048102cdc06b77d7
subhash kumar mandal <pm817405@gmail.com>
2025-09-28 12:30:08 +0530

    questtion 2

---
+++ For-loop/level-1/index.js
@@ -0,0 +1,16 @@
+// ------------------------
+// 🔹 Basics (Level 1)
+// ------------------------
+
+// 1. 1 to 10 number print . 
+for(let i= 1 ; i<11 ;i++){
+    console.log(i)
+}
+
+
+// 2. 1 to 100 print all even number . 
+for (let i =1 ;i<=100;i++){
+    if(i%2===0){
+        console.log(i)
+    }
+}
