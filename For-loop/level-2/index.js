ff60d2b59d14b58326277a1cce63b61a56946949
subhash kumar mandal <pm817405@gmail.com>
2025-09-28 13:33:25 +0530

    question 2

--- For-loop/level-2/index.js
+++ For-loop/level-2/index.js
@@ -8,4 +8,14 @@ let sum = 0
 for(let i=1;i<=n;i++){
     sum = i + sum
 }
-console.log(sum)
+console.log(sum) //21 
+
+// 2. find array maximum element . 
+let arra = [1,3,2,5,6,7,8,9];
+let max = -Infinity;
+for(let i=0;i<arra.length;i++){
+    if(arra[i]>max){
+        max = arra[i]
+    }
+}
+console.log(max) //9
