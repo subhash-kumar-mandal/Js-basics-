858a22acdbed52af27a445ffab34a58c288b4683
subhash kumar mandal <pm817405@gmail.com>
2025-09-28 13:10:19 +0530

    question 8

--- For-loop/level-1/index.js
+++ For-loop/level-1/index.js
@@ -48,3 +48,9 @@ for(let i=1;i<=10;i++){
     console.log(`${i} factorial -> ${factorial}`)
 }
 
+// 8. print the number divisible only 3 . 
+for(let i=1;i<50;i++){
+    if(i%3===0){
+        console.log(i)
+    }
+}
