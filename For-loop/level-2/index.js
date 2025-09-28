89e672277c92a8b7f00d68e4349881f22970013a
subhash kumar mandal <pm817405@gmail.com>
2025-09-28 13:26:43 +0530

    question 1

---
+++ For-loop/level-2/index.js
@@ -0,0 +1,11 @@
+// ---------------------
+// 🔹 Intermediate (Level 2)
+// ---------------------
+
+// 1. sum to all n numbers 
+let n = 6 
+let sum = 0
+for(let i=1;i<=n;i++){
+    sum = i + sum
+}
+console.log(sum)
