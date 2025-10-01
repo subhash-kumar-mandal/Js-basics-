9027b8c7f9c65d6aa6c8bc379fb39298472bdd6b
subhash kumar mandal <pm817405@gmail.com>
2025-10-01 15:17:25 +0530

    scope type

---
+++ js-core-topic/scope/Basic-1/index.js
@@ -0,0 +1,24 @@
+// 1.Scope
+    // ->Scope मतलब variable की visibility और lifetime
+    // ->यानी ये बताता है कि variable कहाँ से access किया जा सकता है 
+
+// ----------> scope type 
+
+//   1.Global Scope 
+//      -अगर variable function या block के बाहर declare किया गया है, तो वो सभी जगह से accessible होता है।
+//      -var, let, const दोनों global scope में काम करते हैं, लेकिन let/const window object में attach नहीं होते।
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+function test() {
+  console.log(a); // 10
+  console.log(b); // 20
+  console.log(c); // 30
+}
+
+test();
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30
