3aec900d6d898a8c09c7be84c26e69eecf660313
subhash kumar mandal <pm817405@gmail.com>
2025-10-01 14:37:38 +0530

    js use case

---
+++ js-core-topic/js-use-case/index.js
@@ -0,0 +1,26 @@
+// -----------------
+// java script core
+// -----------------
+
+// 1. javascript use case 
+// -->Web की Primary Language है
+//    -HTML structure देता है
+//    -CSS styling देता है
+//    -JavaScript behavior और interactivity देती है।
+//    -मतलब browser में directly चलने वाली language है।
+
+// 2. Client-Side + Server-Side दोनों में काम आती है
+//      -पहले सिर्फ browser (front-end) पर use होती थी।
+//      -अब Node.js के बाद server (back-end) पर भी use होती है।
+//      -इसलिए full-stack development में सबसे ज़्यादा demand है।
+
+// 3. Asynchronous Programming सपोर्ट करती है 
+//   -Promises, async/await और callbacks से API calls, real-time updates handle करना आसान है।
+
+// 4.Huge Ecosystem & Community
+//     -बहुत सारे frameworks और libraries हैं: React, Angular, Vue, Node.js, Express, आदि।
+//     -NPM (Node Package Manager) सबसे बड़ा ecosystem है।
+
+// 5.Cross-Platform Use
+// -Web apps, Mobile apps (React Native, Ionic), Desktop apps (Electron), Games, IoT – सब JavaScript से बनाए जा 
+//  सकते हैं।
