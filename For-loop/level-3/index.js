9ee94983677bcf6d74d875562befaa0a8b09567e
subhash kumar mandal <pm817405@gmail.com>
2025-10-06 10:45:51 +0530

    question 1

---
+++ For-loop/level-3/index.js
@@ -0,0 +1,15 @@
+// -----------------------
+// 🔹 Intermediate 3
+// -----------------------
+
+// 1. find sum any arrays
+
+let find_sum  = (arra) => {
+    let sum =0 ;
+    for(let i=0;i<arra.length;i++){
+        sum += arra[i]
+    }
+    return console.log(sum)
+}
+
+find_sum([1,2,3,45,5,6])
