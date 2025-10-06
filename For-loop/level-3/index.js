fdd12c4bd1205403da4f19a1e7e4c3138172b5dd
subhash kumar mandal <pm817405@gmail.com>
2025-10-06 10:54:10 +0530

    find mini element

--- For-loop/level-3/index.js
+++ For-loop/level-3/index.js
@@ -13,3 +13,17 @@ let find_sum  = (arra) => {
 }
 
 find_sum([1,2,3,45,5,6])
+
+// 2. find minimum element in array
+
+let mini = (arra)=>{
+let mini =arra.reduce((curr,sec) =>{
+    if(curr<sec){
+        return curr
+    }else{
+        sec
+    }
+})
+return mini
+}
+console.log(mini([1,2,3,45,6,7]))
